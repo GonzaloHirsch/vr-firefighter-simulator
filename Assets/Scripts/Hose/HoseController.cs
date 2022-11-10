95eb5d981d9e462755ed0490e6175519fb07a8cf
Florencia Petrikovich <fpetrikovich@itba.edu.ar>
2022-11-10 18:58:29 -0300

    feat: hose audio complete

--- Assets/Scripts/Hose/HoseController.cs
+++ Assets/Scripts/Hose/HoseController.cs
@@ -6,14 +6,20 @@ public class HoseController : MonoBehaviour
 {
     public HoseHandleMovement hoseHandle;
     public ParticleSystem hoseWaterPs;
+    public AudioSource audioSource;
+    public AudioClip hoseAudioStart;
+    public AudioClip hoseAudioDuring;
     public float waterDelayTime;
+    public float changeStateInterval;
 
+    private float lastChangeState = 10f;
     private bool hoseIsOn;
 
     // Start is called before the first frame update
     void Awake()
     {
         this.hoseHandle = GameObject.FindGameObjectWithTag("HoseHandle").GetComponent<HoseHandleMovement>();   
+        this.audioSource = this.GetComponent<AudioSource>();
         this.hoseIsOn = false;
         this.HandleHoseWater();
     }
@@ -21,12 +27,43 @@ public class HoseController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!this.hoseHandle.getIsTurning() && ActionMapper.GetDevicesTrigger()) 
+        if (this.IsValidChange() && !this.hoseHandle.getIsTurning() && ActionMapper.GetDevicesTrigger()) 
         {
             this.hoseIsOn = !this.hoseIsOn;
             this.hoseHandle.ActivateHandleMovement(this.hoseIsOn);
+            this.HandleHoseAudio();
             StartCoroutine(this.HandleHoseWaterWithDelay(this.waterDelayTime));
+            this.lastChangeState = 0f;
         }
+        this.lastChangeState += Time.deltaTime;
+    }
+
+    bool IsValidChange() 
+    {
+        return this.lastChangeState >= this.changeStateInterval;
+    }
+
+    void HandleHoseAudio()
+    {
+        if (this.hoseIsOn) {
+            StartCoroutine(this.StartHoseAudio());
+        } else {
+            this.EndHoseAudio();
+        }
+    }
+
+    IEnumerator StartHoseAudio()
+    {
+        this.audioSource.clip = this.hoseAudioStart;
+        this.audioSource.Play();
+        yield return new WaitForSeconds(this.audioSource.clip.length);
+        this.audioSource.clip = this.hoseAudioDuring;
+        this.audioSource.Play();
+    }
+
+    void EndHoseAudio()
+    {
+        StartCoroutine(AudioUtils.FadeOut(this.audioSource, 0.7f));
     }
 
     IEnumerator HandleHoseWaterWithDelay(float delayTime) 
@@ -46,4 +83,5 @@ public class HoseController : MonoBehaviour
             this.hoseWaterPs.Stop();
         }
     }
+
 }
